15c386e9e381599dbb36f8229e3c31e2217855f5
Dan Baechtold <latebloomertech@gmail.com>
2022-07-29 09:48:23 -0400

    basic spotify login in rails

--- app/controllers/AuthController.rb
+++ app/controllers/AuthController.rb
@@ -1,10 +1,21 @@
-# class AuthController < ApplicationController
+class AuthController < ApplicationController
 
 
-# def spotify_request
-#     url = "https://accounts.spotify.com/authorize"
-#     query_params = {
-#         client_id: Rails.application.credentials[Rails.env.to_sym][:spotify][:client_id],
-#         scope: ""
-#     }
-# end
+def spotify_request
+    url = "https://accounts.spotify.com/authorize"
+    query_params = {
+        client_id: 'ec952199a27d436b923a57c0c9e1ea7c',
+        response_type: 'code',
+        redirect_uri:'http://localhost:4000/',
+        scope: 'user-read-private
+        user-read-email
+        user-library-modify
+        playlist-modify-public
+        playlist-modify-private
+        user-top-read
+        user-library-read',
+        show_dialog: true
+    }
+    redirect_to "#{url}?#{query_params.to_query}"
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/hello', to: 'application#hello_world'
-
+  get '/api/v1/login', to: "auth#spotify_request"
   get '*path',
   to: 'fallback#index',
   constraints: ->(req) { !req.xhr? && req.format.html? }
 
   # namespace :api do
   #   namespace :v1 do
-  #     get 'login', to: "auth#spotify_request"
-  #     get 'auth', to: "auth#show"
-  #     get 'user', to: "users#create"
-  #   end
-  # end
+      # get 'auth', to: "auth#show"
+      # get 'user', to: "users#create"
+    # end
 end
